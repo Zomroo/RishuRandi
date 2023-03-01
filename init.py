d58248721839460b1551b9508d85e6e3a30d041e
Zomroo <93994060+Zomroo@users.noreply.github.com>
2023-03-01 17:51:35 +0530

    Update init.py

--- init.py
+++ init.py
@@ -1,5 +1,10 @@
 from pyrogram import Client
 from config import Config
+from pymongo import MongoClient
+from config import MONGO_URI
+
+client = MongoClient(MONGO_URI)
+db = client.randi_database
 
 
 def init_bot():
