0b250d9618b4db1cb5e521b3678b38bb726b73fc
Zomroo <93994060+Zomroo@users.noreply.github.com>
2023-03-01 18:31:08 +0530

    Create database.py

---
+++ database.py
@@ -0,0 +1,57 @@
+from pymongo import MongoClient
+from pymongo.errors import DuplicateKeyError
+
+# Connect to MongoDB
+client = MongoClient("mongodb://localhost:27017/")
+db = client["my_database"]
+
+
+class Database:
+    @staticmethod
+    def save_waifu(chat_id, waifu, message_id):
+        """
+        Save the waifu information in the database.
+        """
+        collection = db["waifus"]
+        data = {"chat_id": chat_id, "waifu": waifu, "message_id": message_id}
+        try:
+            collection.insert_one(data)
+        except DuplicateKeyError:
+            # If the message ID already exists, update the waifu information
+            collection.update_one({"message_id": message_id}, {"$set": {"waifu": waifu}})
+
+    @staticmethod
+    def get_current_waifu(chat_id):
+        """
+        Get the current waifu for the given chat.
+        """
+        collection = db["waifus"]
+        return collection.find_one({"chat_id": chat_id}, sort=[("_id", -1)])
+
+    @staticmethod
+    def add_user(chat_id, user_id):
+        """
+        Add a user to the list of users who have protected the current waifu.
+        """
+        collection = db["users"]
+        data = {"chat_id": chat_id, "user_id": user_id}
+        try:
+            collection.insert_one(data)
+        except DuplicateKeyError:
+            pass  # If the user already exists, do nothing
+
+    @staticmethod
+    def get_users(chat_id):
+        """
+        Get the list of users who have protected the current waifu.
+        """
+        collection = db["users"]
+        return list(collection.find({"chat_id": chat_id}))
+    
+    @staticmethod
+    def get_all_chats():
+        """
+        Get the list of all chat IDs in the database.
+        """
+        collection = db["waifus"]
+        return collection.distinct("chat_id")
