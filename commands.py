f458507693e9468dfb47260b17845774dea9ad4a
Zomroo <93994060+Zomroo@users.noreply.github.com>
2023-03-01 19:11:49 +0530

    Update commands.py

--- commands.py
+++ commands.py
@@ -1,30 +1,38 @@
-from pyrogram import filters
-from waifu import Waifu
-from database import db
+from pyrogram import Client, filters
+from config import ADMIN_ID
 
-@Waifu.app.on_message(filters.command("start"))
-async def start(_, message):
-    await message.reply_text("Hi! I'm your waifu catcher bot.")
+class Commands:
+    app = Client("waifu_catcher_bot")
 
-@Waifu.app.on_message(filters.command("waifu"))
-async def waifu(_, message):
-    waifu_name = message.text.split(" ", 1)[1].lower()
-    waifu = await Waifu.get_waifu(waifu_name)
-    if waifu:
-        await Waifu.send_photo(message.chat.id, waifu["name"], waifu["photo"])
-    else:
-        await message.reply_text("There's no waifu with that name.")
-
-@Waifu.app.on_message(filters.command("mywaifu"))
-async def mywaifu(_, message):
-    user_id = message.from_user.id
-    waifus = db.users.find_one({"user_id": user_id})
-    if waifus:
-        waifus = waifus["waifus"]
-        if waifus:
-            waifu_list = "\n".join([waifu.capitalize() for waifu in waifus])
-            await message.reply_text(f"Your waifus:\n\n{waifu_list}")
+    @app.on_message(filters.command("start", "/"))
+    async def start(_, message):
+        if message.chat.type == "private":
+            await message.reply("Hello! I'm the Waifu Catcher Bot. To catch a waifu, use the command /waifu followed by the waifu's name.")
         else:
-            await message.reply_text("You haven't protecced any waifus yet.")
-    else:
-        await message.reply_text("You haven't protecced any waifus yet.")
+            await message.reply("Please use this bot in private messages only.")
+
+    @app.on_message(filters.command("waifu", "/"))
+    async def waifu(_, message):
+        name = message.text.split(" ", 1)[-1].lower()
+        waifu = await Waifu.get_waifu(name)
+        if waifu is None:
+            await message.reply("Sorry, I don't have that waifu.")
+            return
+        message_id = await Waifu.send_photo(message.chat.id, waifu["name"], waifu["photo"])
+        if message_id is not None:
+            db.pending_messages.insert_one({"message_id": message_id, "waifu_name": waifu["name"], "chat_id": message.chat.id})
+
+    @app.on_message(filters.command("my", "/"))
+    async def mywaifus(_, message):
+        if message.chat.type == "private":
+            user_id = message.from_user.id
+            user = db.users.find_one({"user_id": user_id})
+            if user is not None and "waifus" in user:
+                waifus = "\n- ".join(user["waifus"])
+                await message.reply(f"Your waifus:\n- {waifus}")
+            else:
+                await message.reply("You haven't protecced any waifus yet!")
+        else:
+            await message.reply("Please use this command in private messages only.")
+
+Commands.app.run()
