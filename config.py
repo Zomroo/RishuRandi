806ce40a561c06f27ce8ddf60db9cd1fa640a59f
Zomroo <93994060+Zomroo@users.noreply.github.com>
2023-03-01 19:25:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,16 @@
-import os
-from dotenv import load_dotenv
+# Telegram API configuration
+API_ID = 15849735
+API_HASH = "your_api_hash_here"
 
-load_dotenv()
+# Bot configuration
+BOT_TOKEN = "5562112612:AAH7Sbz2iIAdoPknjv0FnuiNbiDa_5OFYQA"
+BOT_USERNAME = "RoseeBbot"
 
-API_ID = int(os.environ.get("API_ID", "15849735"))
-API_HASH = os.environ.get("API_HASH", "b8105dc4c17419dfd4165ecf1d0bc100")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "5562112612:AAH7Sbz2iIAdoPknjv0FnuiNbiDa_5OFYQA")
-MONGO_URI = os.environ.get("MONGO_URI", "mongodb+srv://Lauda:Lauda@cluster0.niyuza9.mongodb.net/?retryWrites=true&w=majority/")
+# MongoDB configuration
+MONGO_URL = "mongodb+srv://Lauda:Lauda@cluster0.niyuza9.mongodb.net/?retryWrites=true&w=majority/"
+
+# Admin configuration
+ADMIN_ID = 15849735
+
+# Other configuration
+COMMAND_PREFIXES = ["/"]
