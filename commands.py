cb232609dac6dbf64fffa9ae6e49713fbfd34e3c
Zomroo <93994060+Zomroo@users.noreply.github.com>
2023-03-01 19:01:26 +0530

    Update commands.py

--- commands.py
+++ commands.py
@@ -1,78 +1,30 @@
-from pyrogram.types import Message
-from randikhana import Randikhana
-from randi import Randi
-from database import Database
+from pyrogram import filters
+from waifu import Waifu
+from database import db
 
+@Waifu.app.on_message(filters.command("start"))
+async def start(_, message):
+    await message.reply_text("Hi! I'm your waifu catcher bot.")
 
-def start(client: 'Client', message: Message):
-    """
-    Handle the /start command.
-    """
-    client.send_message(
-        chat_id=message.chat.id,
-        text="Hi! I'm a waifu catcher bot. Send me /waifu command to get a random waifu."
-    )
-
-
-def help(client: 'Client', message: Message):
-    """
-    Handle the /help command.
-    """
-    client.send_message(
-        chat_id=message.chat.id,
-        text="This is a waifu catcher bot. Send me /waifu command to get a random waifu. "
-             "To protect the waifu, send /randi followed by the waifu name. "
-             "You can also see the users who have protected the waifu by sending /myrandi."
-    )
-
-
-def waifu(client: 'Client', message: Message):
-    """
-    Handle the /waifu command.
-    """
-    # delete the previous waifu's message
-    prev_waifu = Database.get_current_waifu(message.chat.id)
-    if prev_waifu:
-        client.delete_messages(chat_id=message.chat.id, message_ids=prev_waifu["message_id"])
-
-    # send a new waifu message
-    waifu = Randi.get_random_waifu()
-    message = client.send_photo(chat_id=message.chat.id, photo=waifu["pic"], caption=waifu["name"])
-    Database.save_waifu(chat_id=message.chat.id, waifu=waifu, message_id=message.message_id)
-
-
-def randi(client: 'Client', message: Message):
-    """
-    Handle the /randi command.
-    """
-    # get the waifu name from the command
-    waifu_name = message.text.split(maxsplit=1)[1].strip()
-
-    # get the current waifu and its chat ID
-    current_waifu = Database.get_current_waifu(message.chat.id)
-    if not current_waifu:
-        client.send_message(chat_id=message.chat.id, text="No waifu currently available.")
-        return
-    chat_id = current_waifu["chat_id"]
-
-    # check if the waifu name matches
-    if waifu_name.lower() == current_waifu["waifu"]["name"].lower():
-        client.send_message(chat_id=message.chat.id, text="You have protected the waifu!")
-        Database.add_user(chat_id=chat_id, user_id=message.from_user.id)
+@Waifu.app.on_message(filters.command("waifu"))
+async def waifu(_, message):
+    waifu_name = message.text.split(" ", 1)[1].lower()
+    waifu = await Waifu.get_waifu(waifu_name)
+    if waifu:
+        await Waifu.send_photo(message.chat.id, waifu["name"], waifu["photo"])
     else:
-        client.send_message(chat_id=message.chat.id, text="Wrong waifu name!")
+        await message.reply_text("There's no waifu with that name.")
 
-
-def myrandi(client: 'Client', message: Message):
-    """
-    Handle the /myrandi command.
-    """
-    chat_id = message.chat.id
-    users = Database.get_users(chat_id)
-
-    if not users:
-        client.send_message(chat_id=chat_id, text="No users have protected the waifu yet.")
-        return
-
-    user_list = "\n".join([f"{u['first_name']} ({u['id']})" for u in users])
-    client.send_message(chat_id=chat_id, text=f"Users who have protected the waifu:\n{user_list}")
+@Waifu.app.on_message(filters.command("mywaifu"))
+async def mywaifu(_, message):
+    user_id = message.from_user.id
+    waifus = db.users.find_one({"user_id": user_id})
+    if waifus:
+        waifus = waifus["waifus"]
+        if waifus:
+            waifu_list = "\n".join([waifu.capitalize() for waifu in waifus])
+            await message.reply_text(f"Your waifus:\n\n{waifu_list}")
+        else:
+            await message.reply_text("You haven't protecced any waifus yet.")
+    else:
+        await message.reply_text("You haven't protecced any waifus yet.")
