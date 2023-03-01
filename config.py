bf0d3eee47c4bf9dcdf99adfdc44272f518a1ddc
Zomroo <93994060+Zomroo@users.noreply.github.com>
2023-03-01 17:51:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
-class Config:
-    API_ID = 15849735  # Replace with your Telegram API ID
-    API_HASH = "b8105dc4c17419dfd4165ecf1d0bc100"  # Replace with your Telegram API hash
-    BOT_TOKEN = "5562112612:AAH7Sbz2iIAdoPknjv0FnuiNbiDa_5OFYQA"  # Replace with your bot's token
-    MONGO_URL = "mongodb+srv://Lauda:Lauda@cluster0.niyuza9.mongodb.net/?retryWrites=true&w=majority/"  # Replace with your MongoDB connection URL
+import os
+
+API_ID = int(os.environ.get("15849735"))
+API_HASH = os.environ.get("b8105dc4c17419dfd4165ecf1d0bc100")
+BOT_TOKEN = os.environ.get("5562112612:AAH7Sbz2iIAdoPknjv0FnuiNbiDa_5OFYQA")
+MONGO_URI = os.environ.get("mongodb+srv://Lauda:Lauda@cluster0.niyuza9.mongodb.net/?retryWrites=true&w=majority/")
