f451a101c17ef928d1b84c1fb2e223535d0d15d2
Zomroo <93994060+Zomroo@users.noreply.github.com>
2023-03-01 19:37:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # Telegram API configuration
 API_ID = 15849735
-API_HASH = "your_api_hash_here"
+API_HASH = "b8105dc4c17419dfd4165ecf1d0bc100"
 
 # Bot configuration
 BOT_TOKEN = "5562112612:AAH7Sbz2iIAdoPknjv0FnuiNbiDa_5OFYQA"
