176022aaf385b1236187d90267a667208afd9af1
Zomroo <93994060+Zomroo@users.noreply.github.com>
2023-03-01 19:00:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,14 @@
-import time
-import schedule
 from pyrogram import Client
-from config import API_ID, API_HASH, BOT_TOKEN, MONGO_URI
-from randi import Randi
-from database import Database
+from commands import *
+from config import API_ID, API_HASH, BOT_TOKEN
 
-bot = Client(
-    "my_bot",
+app = Client(
+    "waifu_catcher_bot",
     api_id=API_ID,
     api_hash=API_HASH,
     bot_token=BOT_TOKEN,
+    parse_mode="html"
 )
 
-# Start the bot
-bot.start()
-
-# Set the time interval for sending the waifu announcements
-TIME_INTERVAL = 5 * 60  # 5 minutes in seconds
-
-def send_waifu_announcement():
-    for chat_id in Database.get_all_chats():
-        waifu = Randi.get_random_waifu()
-        message = bot.send_photo(chat_id, photo=waifu["pic"], caption=waifu["name"])
-        Database.save_waifu(chat_id, waifu, message.message_id)
-
-# Schedule the job to send the waifu announcements
-schedule.every(TIME_INTERVAL).seconds.do(send_waifu_announcement)
-
-# Run the job scheduler
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+if __name__ == "__main__":
+    app.run()
