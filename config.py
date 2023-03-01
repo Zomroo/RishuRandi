c1b52484778642da716b0f07a4380d6f0fda75fa
Zomroo <93994060+Zomroo@users.noreply.github.com>
2023-03-01 17:12:20 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+class Config:
+    API_ID = 123456  # Replace with your Telegram API ID
+    API_HASH = "123456abcdef"  # Replace with your Telegram API hash
+    BOT_TOKEN = "123456:abcdefg"  # Replace with your bot's token
+    MONGO_URL = "mongodb://localhost:27017/"  # Replace with your MongoDB connection URL
