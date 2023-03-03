8eef35f3d9d969755654f4394505d79242852129
Zomroo <93994060+Zomroo@users.noreply.github.com>
2023-03-03 18:36:54 +0530

    Update commands.py

--- commands.py
+++ commands.py
@@ -1,50 +1,128 @@
-from waifu import Waifu
+from pyrogram import Client
+from database import db
 
-# Start command
-async def start(update, context):
-    user_id = update.effective_user.id
-    await context.bot.send_message(chat_id=user_id, text="Hi! I'm a waifu catcher bot. Use /help to see the available commands.")
+async def start(message, client: Client):
+    user_id = message.from_user.id
+    user_name = message.from_user.first_name
+    db.add_user(user_id, user_name)
+    await message.reply_text(
+        f"Hi {user_name}! I am your personal waifu catcher. Use /help to see what I can do."
+    )
 
-# Help command
-async def help(update, context):
-    user_id = update.effective_user.id
-    help_text = "Available commands:\n\n"
-    help_text += "/start - Start the bot\n"
-    help_text += "/help - Show this help message\n"
-    help_text += "/waifu waifu_name - Catch a waifu by name\n"
-    help_text += "/randi waifu_name - Protect a waifu by name\n"
-    help_text += "/mywaifus - Show the list of waifus you have caught\n"
-    await context.bot.send_message(chat_id=user_id, text=help_text)
+async def help(message, client: Client):
+    text = """
+    <b>Commands List:</b>
+    /start - Start the bot
+    /help - Get a list of available commands
+    /waifu - Catch a waifu
+    /randi - Protect your waifu from getting caught
+    /mywaifus - List all your waifus
+    """
+    await message.reply_text(text, parse_mode="html")
+async def catch_waifu(message, client: Client):
+    user_id = message.from_user.id
+    user_name = message.from_user.first_name
+    waifu = db.get_waifu(user_id)
 
-# Waifu command
-async def catch_waifu(update, context):
-    # Get the user ID and waifu name from the command arguments
-    user_id = update.effective_user.id
-    waifu_name = ' '.join(context.args).capitalize()
-
-    # Catch the waifu if it is available
-    await Waifu.catch_waifu(update, context, waifu_name, user_id)
-
-# Randi command
-async def protect_waifu(update, context):
-    # Get the user ID and waifu name from the command arguments
-    user_id = update.effective_user.id
-    waifu_name = ' '.join(context.args).capitalize()
-
-    # Protect the waifu if it is available
-    await Waifu.protect_waifu(update, context, waifu_name, user_id)
-
-# Mywaifus command
-async def list_waifus(update, context):
-    # Get the user ID from the update
-    user_id = update.effective_user.id
-
-    # Get the list of caught waifus for this user
-    caught_waifus = await Waifu.get_caught_waifus(update, context, user_id)
-
-    # Send the list of caught waifus to the user
-    if caught_waifus:
-        waifus_text = '\n'.join(caught_waifus)
-        await context.bot.send_message(chat_id=user_id, text=f"You have caught the following waifus:\n{waifus_text}")
+    if waifu:
+        text = f"Sorry {user_name}, you already have a waifu named {waifu}. Use /randi to protect your waifu from getting caught."
     else:
-        await context.bot.send_message(chat_id=user_id, text="You haven't caught any waifus yet.")
+        waifu = db.catch_waifu(user_id, user_name)
+        text = f"Congratulations {user_name}, you have successfully caught a new waifu named {waifu}."
+        
+    await message.reply_text(text)
+
+async def protect_waifu(message, client: Client):
+    user_id = message.from_user.id
+    user_name = message.from_user.first_name
+    waifu = db.get_waifu(user_id)
+
+    if waifu:
+        db.protect_waifu(user_id)
+        text = f"Your waifu {waifu} is now protected from getting caught by other users."
+    else:
+        text = f"Sorry {user_name}, you don't have any waifus to protect. Use /waifu to catch a new waifu."
+        
+    await message.reply_text(text)
+async def list_waifus(message, client: Client):
+    user_id = message.from_user.id
+    user_name = message.from_user.first_name
+    waifus = db.list_waifus(user_id)
+
+    if not waifus:
+        text = f"Sorry {user_name}, you don't have any waifus. Use /waifu to catch a new waifu."
+    else:
+        text = f"Your waifus:\n\n"
+        for index, waifu in enumerate(waifus):
+            text += f"{index+1}. {waifu['name']}\n"
+
+    await message.reply_text(text)
+
+async def release_waifu(message, client: Client):
+    user_id = message.from_user.id
+    user_name = message.from_user.first_name
+    waifu = db.get_waifu(user_id)
+
+    if not waifu:
+        text = f"Sorry {user_name}, you don't have any waifus to release. Use /waifu to catch a new waifu."
+    else:
+        db.release_waifu(user_id)
+        text = f"You have successfully released your waifu {waifu}. Use /waifu to catch a new waifu."
+
+    await message.reply_text(text)
+async def list_waifus(message, client: Client):
+    user_id = message.from_user.id
+    user_name = message.from_user.first_name
+    waifus = db.get_user_waifus(user_id)
+
+    if not waifus:
+        text = f"Sorry {user_name}, you don't have any waifus yet. Use /waifu to catch a new waifu."
+    else:
+        waifu_list = "\n".join([f"- {w}" for w in waifus])
+        text = f"{user_name}, here are all your waifus:\n{waifu_list}"
+        
+    await message.reply_text(text)
+
+
+async def release_waifu(message, client: Client):
+    user_id = message.from_user.id
+    user_name = message.from_user.first_name
+    waifu = db.get_waifu(user_id)
+
+    if not waifu:
+        text = f"Sorry {user_name}, you don't have any waifus to release."
+    else:
+        db.release_waifu(user_id)
+        text = f"{user_name}, your waifu {waifu} has been released and is now available to be caught by other users."
+        
+    await message.reply_text(text)
+
+
+async def rename_waifu(message, client: Client):
+    user_id = message.from_user.id
+    user_name = message.from_user.first_name
+    waifu = db.get_waifu(user_id)
+
+    if not waifu:
+        text = f"Sorry {user_name}, you don't have any waifus to rename."
+    else:
+        new_name = " ".join(message.text.split()[1:])
+        db.rename_waifu(user_id, new_name)
+        text = f"{user_name}, your waifu {waifu} has been renamed to {new_name}."
+        
+    await message.reply_text(text)
+
+async def delete_user_data(message, client: Client):
+    user_id = message.from_user.id
+    user_name = message.from_user.first_name
+    db.delete_user(user_id)
+    text = f"All your data has been deleted, {user_name}."
+    await message.reply_text(text)
+
+
+async def error_handler(update, context):
+    try:
+        raise context.exception
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
