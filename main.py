77839e359895a8d688c55468f5a1a5038a2936a9
Zomroo <93994060+Zomroo@users.noreply.github.com>
2023-03-03 18:41:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from pyrogram import Client, filters
 from pyrogram.types import Message
 from commands import *
 from config import API_ID, API_HASH, BOT_TOKEN
+from database import db
 
 app = Client(
     "waifu_catcher_bot",
@@ -14,6 +15,9 @@ app = Client(
 # Add the command handlers using the `app.on_message()` decorator
 @app.on_message(filters.command("start"))
 async def start_command_handler(client: Client, message: Message):
+    user_id = message.from_user.id
+    user_name = message.from_user.first_name
+    db.add_user(user_id, user_name)
     await start(message, client)
 
 @app.on_message(filters.command("help"))
@@ -22,15 +26,46 @@ async def help_command_handler(client: Client, message: Message):
 
 @app.on_message(filters.command("waifu"))
 async def catch_waifu_command_handler(client: Client, message: Message):
-    await catch_waifu(message, client)
+    user_id = message.from_user.id
+    user_name = message.from_user.first_name
+    waifu = db.get_waifu(user_id)
+
+    if waifu:
+        text = f"Sorry {user_name}, you already have a waifu named {waifu}. Use /randi to protect your waifu from getting caught."
+    else:
+        waifu = db.catch_waifu(user_id, user_name)
+        text = f"Congratulations {user_name}, you have successfully caught a new waifu named {waifu}."
+        
+    await message.reply_text(text)
 
 @app.on_message(filters.command("randi"))
 async def protect_waifu_command_handler(client: Client, message: Message):
-    await protect_waifu(message, client)
+    user_id = message.from_user.id
+    user_name = message.from_user.first_name
+    waifu = db.get_waifu(user_id)
+
+    if waifu:
+        db.protect_waifu(user_id)
+        text = f"Your waifu {waifu} is now protected from getting caught by other users."
+    else:
+        text = f"Sorry {user_name}, you don't have any waifus to protect. Use /waifu to catch a new waifu."
+        
+    await message.reply_text(text)
 
 @app.on_message(filters.command("mywaifus"))
 async def list_waifus_command_handler(client: Client, message: Message):
-    await list_waifus(message, client)
+    user_id = message.from_user.id
+    user_name = message.from_user.first_name
+    waifus = db.get_all_waifus(user_id)
+
+    if waifus:
+        text = f"{user_name}, your waifus:\n\n"
+        for waifu in waifus:
+            text += f"- {waifu}\n"
+    else:
+        text = f"Sorry {user_name}, you don't have any waifus yet. Use /waifu to catch a new waifu."
+        
+    await message.reply_text(text)
 
 if __name__ == "__main__":
     app.run()
