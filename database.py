f6f567b383bfe94502d1f93a0e37d6393b2effb1
Zomroo <93994060+Zomroo@users.noreply.github.com>
2023-03-03 18:51:51 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,38 +1,31 @@
 from pymongo import MongoClient
-from pymongo.collection import Collection
-import pymongo
-from config import MONGO_URI, MONGO_DB_NAME
+from pymongo.errors import DuplicateKeyError
+from config import MONGO_URL, MONGO_DB_NAME
 
-client = pymongo.MongoClient(MONGO_URI)
+client = MongoClient(MONGO_URL)
 db = client[MONGO_DB_NAME]
-
-def get_db():
-    return db
-
+users = db["users"]
 
 class Database:
-    def __init__(self, url, db_name):
-        self.client = MongoClient(url)
-        self.db = self.client[db_name]
-        self.users = self.db.users
-
     def add_user(self, user_id, user_name):
-        user = {"_id": user_id, "name": user_name}
-        self.users.insert_one(user)
+        try:
+            users.insert_one({"_id": user_id, "name": user_name})
+        except DuplicateKeyError:
+            pass # user already exists
 
     def get_user(self, user_id):
-        return self.users.find_one({"_id": user_id})
-
+        return users.find_one({"_id": user_id})
+    
     def catch_waifu(self, user_id, user_name):
         waifu = f"{user_name}'s waifu"
-        self.users.update_one({"_id": user_id}, {"$set": {"waifu": waifu}})
+        users.update_one({"_id": user_id}, {"$set": {"waifu": waifu}})
         return waifu
 
     def protect_waifu(self, user_id):
-        self.users.update_one({"_id": user_id}, {"$set": {"protected": True}})
+        users.update_one({"_id": user_id}, {"$set": {"protected": True}})
 
     def unprotect_waifu(self, user_id):
-        self.users.update_one({"_id": user_id}, {"$unset": {"protected": ""}})
+        users.update_one({"_id": user_id}, {"$unset": {"protected": ""}})
 
     def get_waifu(self, user_id):
         user = self.get_user(user_id)
