b7f2250fcdcc71dcf9de4d1407314b53112f090e
Zomroo <93994060+Zomroo@users.noreply.github.com>
2023-03-03 18:23:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from pyrogram import Client
 from commands import *
-from config import Config
+from config import API_ID, API_HASH, BOT_TOKEN
 
 app = Client(
     "waifu_catcher_bot",
-    api_id=Config.API_ID,
-    api_hash=Config.API_HASH,
-    bot_token=Config.BOT_TOKEN,
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN,
     parse_mode="html"
 )
 
