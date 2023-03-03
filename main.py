ef8c73e1b364af2b0081d290344c5d3fb62d847d
Zomroo <93994060+Zomroo@users.noreply.github.com>
2023-03-03 18:19:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from pyrogram import Client
+from pyrogram.types import Message
 from commands import *
-from config import API_ID, API_HASH, BOT_TOKEN
 
 app = Client(
     "waifu_catcher_bot",
@@ -9,6 +9,27 @@ app = Client(
     bot_token=BOT_TOKEN,
     parse_mode="html"
 )
+
+# Add the command handlers using the `app.on_message()` decorator
+@app.on_message(filters.command("start"))
+async def start_command_handler(client: Client, message: Message):
+    await start(message, client)
+
+@app.on_message(filters.command("help"))
+async def help_command_handler(client: Client, message: Message):
+    await help(message, client)
+
+@app.on_message(filters.command("waifu"))
+async def catch_waifu_command_handler(client: Client, message: Message):
+    await catch_waifu(message, client)
+
+@app.on_message(filters.command("randi"))
+async def protect_waifu_command_handler(client: Client, message: Message):
+    await protect_waifu(message, client)
+
+@app.on_message(filters.command("mywaifus"))
+async def list_waifus_command_handler(client: Client, message: Message):
+    await list_waifus(message, client)
 
 if __name__ == "__main__":
     app.run()
