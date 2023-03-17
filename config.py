ca1ae4d9542933d7ab8e9db3464eb956cc4b7962
Zomroo <93994060+Zomroo@users.noreply.github.com>
2023-03-18 02:35:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,4 +14,4 @@ API_HASH = "b8105dc4c17419dfd4165ecf1d0bc100"
 # MongoDB database configuration
 MONGO_URI = "mongodb+srv://Zoro:Zoro@cluster0.x1vigdr.mongodb.net/?retryWrites=true&w=majority"
 MONGO_DB_NAME = "Cluster0"
-MONGO_COLLECTION_NAME = "waifu"
+MONGO_COLLECTION_NAME = "users"
