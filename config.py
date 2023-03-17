dec680c7ba44a9199be6dac389eeffb6b0ab801a
Zomroo <93994060+Zomroo@users.noreply.github.com>
2023-03-18 01:22:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,8 @@
 import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 # Telegram bot API token
 API_TOKEN = os.environ.get("5562112612:AAH7Sbz2iIAdoPknjv0FnuiNbiDa_5OFYQA")
