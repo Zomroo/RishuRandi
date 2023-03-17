41108edcb0e5b8dcef358b50ab3159b6f7e12cac
Zomroo <93994060+Zomroo@users.noreply.github.com>
2023-03-18 01:34:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,13 +5,13 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Telegram bot API token
-API_TOKEN = os.environ.get("5562112612:AAH7Sbz2iIAdoPknjv0FnuiNbiDa_5OFYQA")
+API_TOKEN = "5562112612:AAH7Sbz2iIAdoPknjv0FnuiNbiDa_5OFYQA"
 
 # Pyrogram API ID and hash
-API_ID = os.environ.get("15849735")
-API_HASH = os.environ.get("b8105dc4c17419dfd4165ecf1d0bc100")
+API_ID = "15849735"
+API_HASH = "b8105dc4c17419dfd4165ecf1d0bc100"
 
 # MongoDB database configuration
-MONGO_URI = os.environ.get("mongodb+srv://Zoro:Zoro@cluster0.x1vigdr.mongodb.net/?retryWrites=true&w=majority")
-MONGO_DB_NAME = os.environ.get("Zoro")
-MONGO_COLLECTION_NAME = os.environ.get("Waifu")
+MONGO_URI = "mongodb+srv://Zoro:Zoro@cluster0.x1vigdr.mongodb.net/?retryWrites=true&w=majority"
+MONGO_DB_NAME = "Cluster0"
+MONGO_COLLECTION_NAME = "waifu"
