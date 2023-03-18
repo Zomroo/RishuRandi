22ef51d47d0382c8d0a1c74d449899c7ddfe9cdc
Zomroo <93994060+Zomroo@users.noreply.github.com>
2023-03-18 12:33:45 +0530

    Update waifu.py

--- waifu.py
+++ waifu.py
@@ -1,11 +1,20 @@
+import random
 import pymongo
 import config
-import random
+import pyrogram
 
 # Connect to MongoDB
 client = pymongo.MongoClient(config.MONGO_URI)
 db = client[config.MONGO_DB_NAME]
 collection = db[config.MONGO_COLLECTION_NAME]
+
+app = pyrogram.Client(
+    session_name=config.SESSION_NAME,
+    api_id=config.API_ID,
+    api_hash=config.API_HASH,
+    bot_token=config.BOT_TOKEN
+)
+
 
 def get_random_waifu():
     """
@@ -19,6 +28,7 @@ def get_random_waifu():
         return None, None
     index = random.randint(0, len(waifus["names"]) - 1)
     return waifus["names"][index], waifus["images"][index]
+
 
 def add_waifu(name: str, image_url: str):
     """
@@ -34,6 +44,7 @@ def add_waifu(name: str, image_url: str):
         upsert=True
     )
 
+
 # Add new waifus here
 add_waifu("Asuna", "https://telegra.ph/file/42bd8eb989a5231e4b2f4.jpg")
 add_waifu("Rem", "https://telegra.ph/file/a320819a5cb9006be7ba9.jpg")
@@ -42,7 +53,20 @@ add_waifu("Megumin", "https://telegra.ph/file/311c1059f54af023d2747.jpg")
 add_waifu("Kanna", "https://telegra.ph/file/bfcf25c726fe40d74b13a.jpg")
 add_waifu("Mikasa", "https://telegra.ph/file/3c1f3b4816e38bf16d7db.jpg")
 
-# Get a random waifu
-name, image_url = get_random_waifu()
-print(f"Selected waifu: {name}")
-print(f"Image URL: {image_url}")
+
+def send_waifu():
+    """
+    Sends a random waifu to all private chats.
+    """
+    name, image_url = get_random_waifu()
+    if not name or not image_url:
+        print("No waifus found in database.")
+        return
+
+    for chat in app.get_dialogs():
+        if chat.chat.type == "private":
+            app.send_photo(chat.chat.id, photo=image_url, caption=name)
+
+
+with app:
+    send_waifu()
