314bbfeb5805a976e60d1152d0072a93f02f768f
Zomroo <93994060+Zomroo@users.noreply.github.com>
2023-03-18 12:30:55 +0530

    Update waifu.py

--- waifu.py
+++ waifu.py
@@ -1,5 +1,6 @@
 import pymongo
 import config
+import random
 
 # Connect to MongoDB
 client = pymongo.MongoClient(config.MONGO_URI)
@@ -40,3 +41,8 @@ add_waifu("Zero Two", "https://telegra.ph/file/9e42b21055b1ebf14cde3.jpg")
 add_waifu("Megumin", "https://telegra.ph/file/311c1059f54af023d2747.jpg")
 add_waifu("Kanna", "https://telegra.ph/file/bfcf25c726fe40d74b13a.jpg")
 add_waifu("Mikasa", "https://telegra.ph/file/3c1f3b4816e38bf16d7db.jpg")
+
+# Get a random waifu
+name, image_url = get_random_waifu()
+print(f"Selected waifu: {name}")
+print(f"Image URL: {image_url}")
