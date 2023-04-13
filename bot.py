0dbefe994bb73e9dc66563ae4206aa203286d872
Zoro <93994060+Zomroo@users.noreply.github.com>
2023-04-14 00:11:57 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,124 +1,69 @@
-import os
-import time
-import pymongo
-import pyrogram
-import random
-import asyncio
 from pyrogram import Client, filters
-from pyrogram.types import Message
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 import config
-from waifu import get_random_waifu
+from database import Database
 
-# Connect to MongoDB
-client = pymongo.MongoClient(config.MONGO_URI)
-db = client[config.MONGO_DB_NAME]
-collection = db[config.MONGO_COLLECTION_NAME]
+# Create a Pyrogram client and connect to MongoDB
+app = Client('my_bot', api_id=config.API_ID, api_hash=config.API_HASH, bot_token=config.BOT_TOKEN)
+db = Database()
 
-# Create the Pyrogram client
-api_id = config.API_ID
-api_hash = config.API_HASH
-bot_token = config.BOT_TOKEN
-app = Client("my_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
+# Define a filter to only allow the owner to use the /add and /done commands
+owner_filter = filters.create(lambda _, __, update: str(update.chat.id) == config.OWNER_CHAT_ID)
 
-# Define a list of waifus
-WAIFU_LIST = []
-
-# Define a command handler
-@app.on_message(filters.command("start"))
-async def start_handler(client: Client, message: Message):
-    # Send a welcome message
-    await message.reply_text("Welcome to the waifu catcher bot!")
-    
-# Define a command handler
-@app.on_message(filters.command("catch"))
-async def catch_handler(client: Client, message: Message):
-    # Get the waifu name from the command
-    waifu_name = message.text.split()[1]
-    
-    # Check if the waifu name is valid
-    if waifu_name not in WAIFU_LIST:
-        await message.reply_text("Invalid waifu name. Please try again.")
-        return
-    
-    # Save the waifu in the user's database
-    user_id = message.from_user.id
-    collection.update_one(
-        {"_id": user_id},
-        {"$addToSet": {"waifus": waifu_name}},
-        upsert=True
-    )
-    
-    # Send a message to confirm the catch
-    await message.reply_text(f"Congratulations! You caught {waifu_name}!")
-    
-# Define a command handler
-@app.on_message(filters.command("mywaifu"))
-async def mywaifu_handler(client, message):
-    # Get the user ID
-    user_id = message.from_user.id
-
-    # Retrieve the user data from the database
-    user_data = collection.find_one({"_id": user_id})
-
-    if user_data is None:
-        # User not found in the database
-        await message.reply_text("You haven't added any waifus yet.")
+# Define a callback function for the /add command
+@app.on_message(owner_filter & filters.command('add', prefixes='/'))
+async def add_waifu(client, message):
+    # Ask for the waifu pic
+    await message.reply('Send me the waifu pic.')
+    waifu_pic = await client.ask(message.chat.id, timeout=60)
+    if not waifu_pic.photo:
+        await message.reply('That was not a photo. Aborting.')
         return
 
-    # Retrieve the waifus list from the user data
-    waifus = user_data.get("waifus", [])
-
-    if not waifus:
-        # Waifus list is empty
-        await message.reply_text("You haven't added any waifus yet.")
+    # Ask for the waifu name
+    await message.reply('What is the waifu name?')
+    waifu_name = await client.ask(message.chat.id, timeout=60)
+    if not waifu_name.text:
+        await message.reply('You did not provide a name. Aborting.')
         return
 
-    # Build the message with the user's waifus
-    waifu_list = "\n- ".join(waifus)
-    message_text = f"Your waifus:\n- {waifu_list}"
+    # Ask for the waifu rarity using a custom keyboard
+    keyboard = InlineKeyboardMarkup([
+        [InlineKeyboardButton('Common', callback_data='common')],
+        [InlineKeyboardButton('Rare', callback_data='rare')],
+        [InlineKeyboardButton('Epic', callback_data='epic')],
+        [InlineKeyboardButton('Legendary', callback_data='legendary')],
+    ])
+    await message.reply('Select the waifu rarity:', reply_markup=keyboard)
+    rarity_data = await client.listen(message.chat.id)
+    rarity = rarity_data.data
 
-    # Reply to the user with the waifus list
-    await message.reply_text(message_text)
-# Define a function to load the waifus list from the database
-async def load_waifus():
-    # Retrieve the waifus list from the database
-    waifus_data = collection.find_one({"_id": "waifus"})
-    if waifus_data is None:
-        # Handle the case where no data was returned
-        print("No waifus found in the database.")
-        return
-    waifus_names = waifus_data.get("names", [])
+    # Save the waifu data to MongoDB
+    waifu_data = {
+        'name': waifu_name.text,
+        'pic': waifu_pic.photo[-1].file_id,
+        'rarity': rarity,
+    }
+    db.add_waifu(waifu_data)
 
-    # Update the global WAIFU_LIST variable
-    global WAIFU_LIST
-    WAIFU_LIST = waifus_names
+    # Send a preview of the waifu data
+    rarity_text = {
+        'common': 'Common',
+        'rare': 'Rare',
+        'epic': 'Epic',
+        'legendary': 'Legendary',
+    }[rarity]
+    caption = f'{waifu_name.text}\n{rarity_text}'
+    await client.send_photo(message.chat.id, waifu_pic.photo[-1].file_id, caption=caption)
 
-# Function to send a random waifu image to a group chat
-def send_waifu():
-    # Get a random waifu from the waifu module
-    waifu = get_random_waifu()
+    # Tell the user to use the /done command to complete the waifu adding process
+    await message.reply('Please use the /done command to complete the waifu adding process.')
 
-    # Send the waifu image to a random group chat where the bot is a member
-    group_chats = [chat for chat in app.getUpdates() if chat.message.chat.type == pyrogram.Chat.PRIVATE]
-    if not group_chats:
-        print("Bot is not a member of any group chat.")
-        return
+# Define a callback function for the /done command
+@app.on_message(owner_filter & filters.command('done', prefixes='/'))
+async def finish_add_waifu(client, message):
+    # Tell the user that the waifu adding process is complete
+    await message.reply('Waifu adding process complete.')
 
-    group_chat = random.choice(group_chats).message.chat
-    app.send_photo(chat_id=group_chat.id, photo=waifu["image_url"], caption=f"Here's your waifu: {waifu['name']}")
-
-# Start the client
-if __name__ == "__main__":
-    # Load the waifus list from the database
-    asyncio.get_event_loop().run_until_complete(load_waifus())
-
-    # Start the Pyrogram client
-    app.start()
-
-    # Send waifus every 10 seconds
-    while True:
-        send_waifu()
-        time.sleep(10)
-
-    # Stop the client
-    app.stop()
+# Start the Pyrogram client
+app.run()
