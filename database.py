8d53f4545665c07986fe47c210667b92fcaa1e90
Zoro <93994060+Zomroo@users.noreply.github.com>
2023-04-14 00:43:17 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,14 +1,11 @@
 import os
 import pymongo
+from pymongo import MongoClient
 
+mongo_url = os.environ.get('mongodb+srv://Zoro:Zoro@cluster0.x1vigdr.mongodb.net/?retryWrites=true&w=majority')
+mongo_client = MongoClient(mongo_url)
+db = mongo_client['waifus']
+collection = db['images']
 
-def connect_to_database():
-    # Create MongoDB client instance and connect to database
-    mongo_client = pymongo.MongoClient(os.environ.get("mongodb+srv://Zoro:Zoro@cluster0.x1vigdr.mongodb.net/?retryWrites=true&w=majority"))
-    db = mongo_client["waifu_db"]
-    return db
-
-
-def save_waifu(db, name, rarity, image):
-    # Save waifu in database
-    db.waifus.insert_one({"name": name, "rarity": rarity, "image": image})
+def save_image(file_id):
+    collection.insert_one({'file_id': file_id})
