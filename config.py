f5cbeacbd7261ed5fc41b9a81e61eb74147a317b
Zoro <93994060+Zomroo@users.noreply.github.com>
2023-05-17 19:56:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,9 +7,6 @@ class Config:
     API_ID = 14091414
     API_HASH = os.environ.get('API_HASH', '1e26ebacf23466ed6144d29496aa5d5b')
     BOT_TOKEN = os.environ.get('BOT_TOKEN', '5615528335:AAHOlk2j2TE5CWOv24mxBwpBMAx2ui3Zv1k')
-    OWNER_ID = os.environ.get('OWNER_CHAT_ID', '5500572462')
 
     # MongoDB settings
-    MONGO_URI = os.environ.get('MONGO_URI', 'mongodb+srv://Zoro:Zoro@cluster0.x1vigdr.mongodb.net/?retryWrites=true&w=majority')
-    DB_NAME = os.environ.get('DB_NAME', 'Waify')
-    MONGO_COLLECTION_NAME = os.environ.get('COLLECTION_NAME', 'Khana')
+    MONGO_URL = os.environ.get('MONGO_URL', 'mongodb+srv://Zoro:Zoro@cluster0.x1vigdr.mongodb.net/?retryWrites=true&w=majority')
