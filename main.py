8ccbdecb35e019a59247f583160e36de863845c1
Zoro <93994060+Zomroo@users.noreply.github.com>
2023-05-17 20:16:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import pyrogram
-
 from pyrogram import Client, filters
-from pyrogram.types import Message
-
+from pyrogram.types import Message, Chat
 
 # Create a new bot using your API ID and API Hash
 api_id = 14091414
@@ -15,7 +13,7 @@ app = Client("my_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
 @app.on_message(filters.command(["add"]))
 def add_command_handler(client: Client, message: Message):
     # Check if the command is invoked in a bot chat
-    if isinstance(message.chat, pyrogram.types.PrivateChat):
+    if isinstance(message.chat, Chat) and message.chat.type == "private":
         # Ask the user for a picture
         client.send_message(
             chat_id=message.chat.id,
