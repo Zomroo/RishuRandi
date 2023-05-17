427ac2fa02e02a17a6b281a7f9e8f9cb5ed220c8
Zoro <93994060+Zomroo@users.noreply.github.com>
2023-05-17 20:26:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,42 +1,37 @@
 import pyrogram
 
-from pyrogram import Client, filters
-from pyrogram.types import Message
-
-
-# Create a new bot using your API ID and API Hash
+# Get your API ID, hash, and bot token from @BotFather
 api_id = 14091414
 api_hash = '1e26ebacf23466ed6144d29496aa5d5b'
 bot_token = '5615528335:AAHOlk2j2TE5CWOv24mxBwpBMAx2ui3Zv1k'
 
-app = Client("my_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
+# Create a Pyrogram client
+client = pyrogram.Client(
+    "my_bot",
+    api_id=api_id,
+    hash=hash,
+    bot_token=bot_token,
+)
 
-
-@app.on_message(filters.command(["add"]))
-def add_command_handler(client: Client, message: Message):
-    # Check if the command is invoked in a bot chat
-    if message.chat.type == 'private':
+# Define a command handler for the "add" command
+@client.on_message(filters=filters.command("add"))
+async def add_command(client, message):
+    # Check if the message was sent in a bot command
+    if message.chat.type == "private":
         # Ask the user for a picture
-        client.send_message(
-            chat_id=message.chat.id,
-            text="Please send me a picture to add."
-        )
-    else:
-        # The "add" command is only allowed in private chats
-        client.send_message(
-            chat_id=message.chat.id,
-            text="The 'add' command can only be used in private chats."
-        )
+        await message.reply("Please send me a picture.")
 
+        # Get the picture from the user
+        photo = await message.photo
 
-@app.on_message(filters.photo)
-def photo_handler(client: Client, message: Message):
-    # Handle the received photo here
-    # You can access the photo using `message.photo` attribute
-    client.send_message(
-        chat_id=message.chat.id,
-        text="Thank you for the picture!"
-    )
+        # Save the picture to the bot's filesystem
+        await photo.download("./my_picture.jpg")
 
+        # Send the user a confirmation message
+        await message.reply("I have saved your picture.")
 
-app.run()
+# Start the bot
+client.start()
+
+# Run the bot until it is stopped
+client.run_until_disconnected()
