d46e8718c964c2cd80b2f027493506542d404e26
Zoro <93994060+Zomroo@users.noreply.github.com>
2023-05-17 19:20:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,13 @@
 import os
 
-# Telegram API settings
-API_ID = int(os.environ.get('API_ID', '15849735'))
-API_HASH = os.environ.get('API_HASH', 'b8105dc4c17419dfd4165ecf1d0bc100')
-BOT_TOKEN = os.environ.get('BOT_TOKEN', '5615528335:AAHOlk2j2TE5CWOv24mxBwpBMAx2ui3Zv1k')
-OWNER_ID = os.environ.get('OWNER_CHAT_ID', '5148561602')
+class Config:
+    # Telegram API settings
+    API_ID = int(os.environ.get('API_ID', '15849735'))
+    API_HASH = os.environ.get('API_HASH', 'b8105dc4c17419dfd4165ecf1d0bc100')
+    BOT_TOKEN = os.environ.get('BOT_TOKEN', '5615528335:AAHOlk2j2TE5CWOv24mxBwpBMAx2ui3Zv1k')
+    OWNER_ID = os.environ.get('OWNER_CHAT_ID', '5148561602')
 
-# MongoDB settings
-MONGO_URI = os.environ.get('MONGO_URI', 'mongodb+srv://Zoro:Zoro@cluster0.x1vigdr.mongodb.net/?retryWrites=true&w=majority"')
-DB_NAME = os.environ.get('DB_NAME', 'Waify')
-MONGO_COLLECTION_NAME = os.environ.get('COLLECTION_NAME', 'Khana')
+    # MongoDB settings
+    MONGO_URI = os.environ.get('MONGO_URI', 'mongodb+srv://Zoro:Zoro@cluster0.x1vigdr.mongodb.net/?retryWrites=true&w=majority"')
+    DB_NAME = os.environ.get('DB_NAME', 'Waify')
+    MONGO_COLLECTION_NAME = os.environ.get('COLLECTION_NAME', 'Khana')
