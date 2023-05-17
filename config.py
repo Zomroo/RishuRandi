7dbf192ad089742d2ad821a9e0c4fd265ec2c0a5
Zoro <93994060+Zomroo@users.noreply.github.com>
2023-05-17 19:33:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config:
     API_ID = int(os.environ.get('API_ID', '14091414'))
     API_HASH = os.environ.get('API_HASH', '1e26ebacf23466ed6144d29496aa5d5b')
     BOT_TOKEN = os.environ.get('BOT_TOKEN', '5615528335:AAHOlk2j2TE5CWOv24mxBwpBMAx2ui3Zv1k')
-    OWNER_ID = os.environ.get('OWNER_CHAT_ID', '5148561602')
+    OWNER_ID = os.environ.get('OWNER_CHAT_ID', '5500572462')
 
     # MongoDB settings
     MONGO_URI = os.environ.get('MONGO_URI', 'mongodb+srv://Zoro:Zoro@cluster0.x1vigdr.mongodb.net/?retryWrites=true&w=majority"')
